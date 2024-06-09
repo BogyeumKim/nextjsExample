82a21d366aed74fed17a3e6a2b0f8059cb6487b9
bogyeumkim <namastae112@gmail.com>
2024-06-09 18:55:06 +0900

    test ..

---
+++ components/main-header.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+import logoImg from '@/'
+
+function MainHeader() {
+    return <header>
+        <Link href="/">
+        {/* <img src={} */}
+        </Link>
+    </header>
+}
+
+export default MainHeader;
