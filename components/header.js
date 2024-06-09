72b32c2af7126ac282024f137d24858aef10d905
bogyeumkim <namastae112@gmail.com>
2024-06-09 18:30:43 +0900

    feature : next 기초

---
+++ components/header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <img src="/logo.png" alt="A server surround" />
+      <h1>Welcome to this NextJS Course !!!!!!!!!!!!!!!!</h1>
+    </>
+  );
+}
+
+export default Header;
