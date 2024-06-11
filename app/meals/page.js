7471c2c56055db68f3f060ab9fc3cdcc857b6e67
bogyeumkim <namastae112@gmail.com>
2024-06-11 21:26:07 +0900

    430 sqlite 사용한 Item 렌더링

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
-function MealsPage() {
+import { getMeals } from "@/lib/meals";
+async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -15,8 +18,7 @@ function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-      <MealsGrid meals={[]} />
-
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
