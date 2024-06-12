50e612fd402a6c91be8f0f2df6092208a24ad77a
bogyeumkim <namastae112@gmail.com>
2024-06-12 21:47:24 +0900

    439. 이미지 썸네일출력

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,15 +1,37 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+
   const imageInputRef = useRef();
   const handlePickClick = () => {
     imageInputRef.current.click();
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+        setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+        setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+
   };
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+            {!pickedImage && <p>No image picked yet.</p>}
+            {pickedImage && <Image src={pickedImage} alt="The image" fill />}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -17,6 +39,8 @@ function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInputRef}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
