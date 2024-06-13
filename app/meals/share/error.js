cb2775c902b75d2914089ef2b57926f864c692c0
bogyeumkim <namastae112@gmail.com>
2024-06-13 22:16:15 +0900

    446. 서버사이드 유효성 체크

---
+++ app/meals/share/error.js
@@ -0,0 +1,11 @@
+"use client";
+function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to Create Meal.</p>
+    </main>
+  );
+}
+
+export default Error;
